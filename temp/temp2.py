6cbfa994aa1ef4f9627f1cc182f744e5bfd28b79
getwildysy <42105084+getwildysy@users.noreply.github.com>
2025-12-18 10:32:59 +0800

    bug fix

--- temp/temp2.py
+++ temp/temp2.py
@@ -1,5 +1,3 @@
-from typing import List
-
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
         """
@@ -12,13 +10,4 @@ class Solution:
                 nums_1.append(nums[i])
             else:
                 nums_2.append(nums[i])
-        
-        # 使用切片賦值 (Slice Assignment) 來修改原始 List
-        nums[:] = nums_1 + nums_2
-
-if __name__ == "__main__":
-    solution = Solution()
-    test_nums = [0, 1, 0, 3, 12]
-    print(f"Before: {test_nums}")
-    solution.moveZeroes(test_nums)
-    print(f"After:  {test_nums}")
+        nums = nums_1+nums_2
